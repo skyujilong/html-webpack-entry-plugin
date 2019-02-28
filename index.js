4dd709bb7c2ec6a571e17d03108ba43e62c35db8
skyujilong <skyujilong@163.com>
2019-02-28 19:13:32 +0800

    添加备注，后边进行修改

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ class HtmlWebpackEntryPlugin {
     apply(compiler) {
         compiler.hooks.compilation.tap(this.pluginName, (compilation) => {
             HtmlWebpackPlugin.getHooks(compilation).alterAssetTags.tapAsync('beforeAssetTagGeneration', (data, cb) => {
+                //TODO: 有个link css 也发生了同样的问题！ 我日哦！！！还的修改
                 debugger;
                 // 获取入口名字
                 let entryName = this.getEntryName(data);
@@ -46,7 +47,6 @@ class HtmlWebpackEntryPlugin {
                     }
                 });
             })
-
         });
         return renderJsList;
     }
