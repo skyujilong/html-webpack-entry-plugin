714c9fc77ef6604553de162dc24c02816a0ad4eb
skyujilong <skyujilong@163.com>
2019-02-28 19:10:22 +0800

    fixed 入口js文件没有被加入的问题

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-class HtmlWebpackEntryPlugin{
-    constructor(){
+class HtmlWebpackEntryPlugin {
+    constructor() {
         this.pluginName = 'html-webpack-entry-plugin';
     }
-    apply(compiler){
+    apply(compiler) {
         compiler.hooks.compilation.tap(this.pluginName, (compilation) => {
             HtmlWebpackPlugin.getHooks(compilation).alterAssetTags.tapAsync('beforeAssetTagGeneration', (data, cb) => {
                 debugger;
                 // 获取入口名字
                 let entryName = this.getEntryName(data);
 
-                for (let item of compilation.entrypoints){
+                for (let item of compilation.entrypoints) {
                     // key值为入口名字，entryPoint中，含有整个html中需要的js文件。
                     let key = item[0],
                         entryPoint = item[1];
-                    if(key === entryName){
+                    if (key === entryName) {
                         data.assetTags.scripts = this.getAssetTagsScripts(entryPoint.chunks, data.assetTags.scripts);
                         break;
                     }
@@ -24,7 +24,7 @@ class HtmlWebpackEntryPlugin{
             })
         });
     }
-    getEntryName(data){
+    getEntryName(data) {
         let entryName = data.plugin.options.filename.split('.')[0];
         return entryName;
     }
@@ -39,12 +39,14 @@ class HtmlWebpackEntryPlugin{
         // let mode = compilation.options.mode;
         let renderJsList = [];
         entryChunkList.forEach(chunk => {
-            let renderJsName = chunk.files[0];
-            assetTagsScripts.forEach(htmlScript => {
-                if (htmlScript.attributes.src.indexOf(renderJsName) != -1){
-                    renderJsList.push(htmlScript);
-                }
-            });
+            chunk.files.forEach(renderJsName => {
+                assetTagsScripts.forEach(htmlScript => {
+                    if (htmlScript.attributes.src.indexOf(renderJsName) != -1) {
+                        renderJsList.push(htmlScript);
+                    }
+                });
+            })
+
         });
         return renderJsList;
     }
